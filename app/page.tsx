ebaa91e058bdb1f183f1c0ce19b76a0440692862
R Amarthya Sreechand <amarthyasreechand@gmail.com>
2026-03-04 16:28:31 +0530

    Add Css

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 
 export default function Home() {
+
     return (
         // only the divs & section (block elements) are given styles. 
         // So put every style inside div
@@ -8,17 +9,16 @@ export default function Home() {
             <div className="container">
                 <section className="hero">
                     <canvas></canvas>
+
                     <div className="hero-content">
                         <div className="header">
                             <h1>Where Beauty Begins</h1>
                             <p>Trusted by</p>
                             <div className="client-logos">
-                                <div className="client-logo">
                                     <div className="client-logo"><img src="/clients/client-logo-1.svg" alt="img1" /></div>
                                     <div className="client-logo"><img src="/clients/client-logo-2.svg" alt="img2" /></div>
                                     <div className="client-logo"><img src="/clients/client-logo-3.svg" alt="img3" /></div>
                                     <div className="client-logo"><img src="/clients/client-logo-4.svg" alt="img4" /></div>
-                                </div>
                             </div>
                         </div>
                     </div>
